06f5639df6ab703bf0a92c099d42d5f1ae37449c
LabReact1 <linhnmfx12988@funix.edu.vn>
2021-11-20 10:09:57 +0700

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>  
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+ 
+
+
